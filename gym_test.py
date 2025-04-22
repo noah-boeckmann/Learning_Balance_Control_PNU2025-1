c4c420a208944e2463dbb4f07290a965fb7e1cf0
Noah Böckmann <kontakt@noahb.de>
2025-04-22 11:43:29 +0900

    added a rigid leg wheelbot model, no actuators

--- gym_test.py
+++ gym_test.py
@@ -1,3 +1,5 @@
+import math
+
 import gymnasium as gym  # open ai gym
 import time
 
@@ -8,14 +10,32 @@ import robot_gym
 def main():
     gym.register('WheelBot', robot_gym.WheelBotEnv)
 
-    env = gym.make("WheelBot", reset_noise_scale=0.1, render_mode="human")
+    env = gym.make("WheelBot", xml_file="./bot_model/wheelbot_rigid.xml", reset_noise_scale=0.0, render_mode="human", frame_skip=1, width=1000, height=1000)
     #env.render() # call this before env.reset, if you want a window showing the environment
 
     env.reset()  # should return a state vector if everything worked
     env.render()
+
+    t = 0.0
+
+    a = []
     while True:
-        env.step(env.action_space.sample())
-        time.sleep(0.05)
+        env.reset()
+        for i in range(100):
+            env.render()
+            time.sleep(0.01)
+
+        for i in range(100):
+            env.step(a)
+            time.sleep(0.05)
+
+    h_range = 0.5 * (1.5708 - 0.6980)
+    while True:
+        a = h_range + h_range * math.sin(math.pi * t * 0.25)
+        action = [-1.5708 + a, 1.5708 - a, 1.5708 - a, -1.5708 + a]
+        t = t + 0.01
+        env.step(action)
+        time.sleep(0.01)
 
 
 if __name__ == '__main__':
